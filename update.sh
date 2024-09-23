11c62b49b59aa3e713bb0cb37f1fc6b1b16bba17
Sini921 <1666260210@qq.com>
2024-09-23 22:21:59 +0800

    update repo

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ dpkg-scanpackages --multiversion roothide >> Packages
 
 cat Packages | xz > Packages.xz
 cat Packages | bzip2 > Packages.bz2
+cat Packages | gzip > Packages.gz
 
 git add .
 git commit -m "update repo"
