581ac7992cbc76c3db9fef2d7d40e4fbb5ded198
Sini921 <1666260210@qq.com>
2024-09-23 19:04:17 +0800

    update repo

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+dpkg-scanpackages --multiversion rootful > Packages
+dpkg-scanpackages --multiversion rootless >> Packages
+dpkg-scanpackages --multiversion roothide >> Packages
+
+cat Packages | xz > Packages.xz
+cat Packages | bzip2 > Packages.bz2
+
+git add .
+git commit -m "update repo"
+git push
