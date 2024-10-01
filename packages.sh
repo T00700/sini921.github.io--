cbc8a0c49b41360143f49712ab4d82172579fa75
Sini921 <1666260210@qq.com>
2024-10-01 16:04:45 +0800

    update repo

---
+++ packages.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#dpkg-scanpackages --multiversion rootful > Packages
+dpkg-scanpackages --multiversion rootless >> Packages
+dpkg-scanpackages --multiversion roothide >> Packages
+
+cat Packages | xz > Packages.xz
+cat Packages | bzip2 > Packages.bz2
+#cat Packages | gzip > Packages.gz
+#cat Packages | lzma > Packages.lzma
+#cat Packages | zstd > Packages.zst
+
+#git add .
+#git commit -m "update repo"
+#git push
