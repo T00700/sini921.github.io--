3b7bf5e48c8d85e537d8cbfa35096b408a8a9282
Sini921 <1666260210@qq.com>
2024-10-06 02:19:33 +0800

    update repo

--- packages.sh
+++ packages.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-rm Packages
+rm Packages Packages.bz2 Packages.xz
 
 #dpkg-scanpackages --multiversion rootful > Packages
 dpkg-scanpackages --multiversion rootless >> Packages
 dpkg-scanpackages --multiversion roothide >> Packages
 
-cat Packages | xz > Packages.xz
-cat Packages | bzip2 > Packages.bz2
+#cat Packages | xz > Packages.xz
+#cat Packages | bzip2 > Packages.bz2
 #cat Packages | gzip > Packages.gz
 #cat Packages | lzma > Packages.lzma
 #cat Packages | zstd > Packages.zst
