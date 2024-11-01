7fb0c2294dd64160b0a5ff3033c2d61cbd30d923
Sini921 <1666260210@qq.com>
2024-11-01 14:32:36 +0800

    update repo

--- update.sh
+++ update.sh
@@ -9,6 +9,17 @@ cat Packages | bzip2 > Packages.bz2
 #cat Packages | lzma > Packages.lzma
 #cat Packages | zstd > Packages.zst
 
+apt-ftparchive\
+ -o APT::FTPArchive::Release::Origin="- 季伯之家"\
+ -o APT::FTPArchive::Release::Label="- 季伯之家"\
+ -o APT::FTPArchive::Release::Suite="stable"\
+ -o APT::FTPArchive::Release::Version="1.0"\
+ -o APT::FTPArchive::Release::Codename="ios"\
+ -o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64 iphoneos-arm64e"\
+ -o APT::FTPArchive::Release::Components="main"\
+ -o APT::FTPArchive::Release::Description="源内插件仅为自用备份"\
+ release . > Release
+
 git add .
 git commit -m "update repo"
 git push
